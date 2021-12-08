cd338aa5ac55f1f1ff5e4788afec3ff734406481
Juan Carlos Andrade <jandradeu@unal.edu.co>
2021-12-08 10:47:29 -0500

    Se añaden las clases para la lista de tareas

--- Reminder_alpha/base/models.py
+++ Reminder_alpha/base/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.fields import BLANK_CHOICE_DASH
 # Create your models here.
+# Notes Clases.
 class Notes(models.Model):
     STATUS = (
         ("new", "NEWEST"),
@@ -14,3 +16,17 @@ class Notes(models.Model):
     def __str__(self):
         return self.heading
 
+#Task List Clases.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank= True)
+    complete = models.BooleanField(default=False)
+    create = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['Complete']
