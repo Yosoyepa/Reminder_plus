03695e15b24150546758ddd2dbe7ff2cf8fda407
Juan Carlos Andrade <jandradeu@unal.edu.co>
2021-12-07 20:26:50 -0500

    version en donde funciona new_notes

--- Reminder_alpha/base/urls.py
+++ Reminder_alpha/base/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.tasklist, name='tasks'),
-    path('', views.index, name='index'),
-    path('new_note', views.new_note, name='new_note'),
+    path('', views.index, name = 'index'),
+    path('new_note', views.new_note, name = 'new'),
+    path('note/<str:pk>', views.note_detail, name = 'note'),
+    path('delete_note/<str:pk>', views.delete_note, name = 'delete'),
+    path('search_result', views.search_page, name = 'search'),
+
 ]
--- Reminder_alpha/base/views.py
+++ Reminder_alpha/base/views.py
@@ -1,12 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Notes
+from .forms import NotesForm
+from django.contrib import messages
+# Create your views here.
 
-## General Views For Users:
+
 def index(request):
-    return render(request, 'index.html', {})
+    notes = Notes.objects.all()
+    return render(request, "index.html", {"notes": notes})
 
 def new_note(request):
-    return render(request, 'new_note.html', {})
+    form = NotesForm()
+    if request.method == 'POST':
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    return render(request, "update.html", {"form": form})
 
-def tasklist(request):
-    return HttpResponse('To do List')
+def note_detail(request, pk):
+    note = Notes.objects.get(id=pk)
+    form = NotesForm(instance=note)
+    if request.method == 'POST':
+        form = NotesForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    return render(request, "update.html", {"note": note, "form": form})
+
+
+def delete_note(request, pk):
+    note = Notes.objects.get(id=pk)
+    form = NotesForm(instance=note)
+    if request.method == 'POST':
+        note.delete()
+        messages.info(request, "La nota ha sido eliminada")
+    return render(request, "delete.html", {"note": note, "form": form})
+
+def search_page(request):
+    if request.method == 'POST':
+        search_text = request.POST['search']
+        notes = Notes.objects.filter(heading__icontains = search_text) | Notes.objects.filter(text__icontains = search_text)
+        # if notes is None:
+        #     messages.info(request, "la nota no se ha encontrado")
+        return render(request, "search.html", {"notes": notes})
